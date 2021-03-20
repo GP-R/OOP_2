7a1f17edd90221f0f758c624f75cb42cb772c9ee
GP-R <48301097+GP-R@users.noreply.github.com>
2021-03-21 02:11:55 +0900

    test
    test

---
+++ TEST/TEST/Ranking.h
@@ -0,0 +1,18 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<stdlib.h>
+#define MAX_RANK 5
+struct PlayInfo {
+	char name[80];
+	int nMove;
+	double tElapsed;
+};
+
+struct Rank {
+	void loadRanking(PlayInfo player[], int len, const char *filename);
+	void storeRanking(PlayInfo player[], int len, const char *filename);
+	void printRanking(PlayInfo player[], int len);
+	int addRanking(PlayInfo player[], int len, int nmove, double elapsed);
+	void initRanking(PlayInfo player[], int len);
+};
